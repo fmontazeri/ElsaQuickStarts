77dd018a4d008740662338177b52facb532aa7d8
f.montazeri <montazeri.fm@gmail.com>
2024-09-25 15:16:21 +0330

    elsa with dotnet 6

--- ElsaQuickstarts.WebApp.HelloWorld/Program.cs
+++ ElsaQuickstarts.WebApp.HelloWorld/Program.cs
@@ -1,5 +1,7 @@
 using ElsaQuickstarts.WebApp.HelloWorld.Workflows;
 
+//https://localhost:7211/hello-world
+
 var builder = WebApplication.CreateBuilder(args);
 //Unhandled exception. System.InvalidOperationException: The service collection cannot be modified because it is read-only.
 //app.UseHttpActivities();
