ea6322d91508a66d8a3702f6e381edbf2b52d272
f.montazeri <montazeri.fm@gmail.com>
2024-09-25 14:22:35 +0330

    add-git-ignore

---
+++ ElsaQuickstarts.ConsoleApp.HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Elsa.Services;
+using ElsaQuickstarts.ConsoleApp.HelloWorld.Workflows;
+using Microsoft.Extensions.DependencyInjection;
+
+
+// Create a service container with Elsa services.
+var services = new ServiceCollection()
+    .AddElsa(options => options
+        .AddConsoleActivities()
+        .AddWorkflow<HelloWorld>())
+    .BuildServiceProvider();
+            
+// Get a workflow runner.
+var workflowRunner = services.GetRequiredService<IBuildsAndStartsWorkflow>();
+
+// Run the workflow.
+await workflowRunner.BuildAndStartWorkflowAsync<HelloWorld>();
---
+++ ElsaQuickstarts.ConsoleApp.HelloWorld/Workflows/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Builders;
+
+namespace ElsaQuickstarts.ConsoleApp.HelloWorld.Workflows;
+
+public class HelloWorld : IWorkflow
+{
+    //The below workflow has only one step (a.k.a. activity): WriteLine
+    public void Build(IWorkflowBuilder builder) => builder.WriteLine("Hello World!");
+}
+    
