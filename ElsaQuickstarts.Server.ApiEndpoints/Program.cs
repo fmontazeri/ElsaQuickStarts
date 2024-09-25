8cb8f1d85ecb6364659ae593e529627196845291
f.montazeri <montazeri.fm@gmail.com>
2024-09-25 18:08:30 +0330

    add some comments

--- ElsaQuickstarts.Server.ApiEndpoints/Program.cs
+++ ElsaQuickstarts.Server.ApiEndpoints/Program.cs
@@ -2,15 +2,19 @@ using Elsa;
 using Elsa.Persistence.EntityFramework.Core.Extensions;
 using Elsa.Persistence.EntityFramework.Sqlite;
 
+//In this project, we've seen how to setup an Elsa Server Host
+//that can host and execute workflows and exposes a set of API endpoints for use by external applications
+
 var builder = WebApplication.CreateBuilder(args);
 var elsaSection = builder.Configuration.GetSection("Elsa");
 
 // Elsa services.
 builder.Services
     .AddElsa(elsa => elsa
-        .UseEntityFrameworkPersistence(ef =>  ef.UseSqlite())
+        .UseEntityFrameworkPersistence(ef => ef.UseSqlite())
         .AddConsoleActivities()
-        .AddHttpActivities(elsaSection.GetSection("Server").Bind)// TODO: port 5001 doesn't work; we used port 5187 instead to get workflow data.
+        .AddHttpActivities(elsaSection.GetSection("Server")
+            .Bind) // TODO: port 5001 doesn't work; we used port 5187 instead to get workflow data.
         .AddQuartzTemporalActivities()
         .AddJavaScriptActivities()
         .AddWorkflowsFrom<Program>()
@@ -55,5 +59,5 @@ app
     });
 
 
-
 app.Run();
+
